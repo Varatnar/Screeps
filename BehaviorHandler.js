8e4dda5351f0c9cd217d5053cef30adf6010f255
Philippe Leclerc <philippeleclerc299@gmail.com>
2017-07-30 16:55:14 -0400

    Basic flow of a harvester

--- BehaviorHandler.js
+++ BehaviorHandler.js
@@ -14,9 +14,42 @@ module.exports = function () {
     Creep.prototype.act = function () {
         let self = this;
 
+        if(self.memory.role == cst.ROLE_HARVESTER) {
+            self.say("!!");
+            if(self.memory.task == undefined) {
+                self.memory.task = cst.STATE_HARVESTING_SOURCE;
+            }
+            self.doTask();
+            if(self.memory.task == cst.STATE_AWAITING_CHANGE) {
+                self.findOutNextTask();
+            }
+        }
+
     };
 
-    function assignTask() {
+    /**
+     * Mapping of constant to actual task
+     */
+    Creep.prototype.doTask = function() {
+        let self = this;
 
-    }
+        switch (self.memory.task) {
+            case cst.STATE_HARVESTING_SOURCE: self.harvestSource(); break;
+            case cst.STATE_STORING_ENERGY_FOR_USE: self.bringEnergyBack(); break;
+            default: console.log("ERROR WHEN DOING TASK");
+        }
+    };
+
+    Creep.prototype.findOutNextTask = function() {
+        let self = this;
+
+        //todo change for pwetty stuff, this is awfull !!! CHANGE
+
+        if(self.memory.lastTask == cst.STATE_HARVESTING_SOURCE) {
+            self.memory.task = cst.STATE_STORING_ENERGY_FOR_USE;
+        } else {
+            self.memory.task = cst.STATE_HARVESTING_SOURCE;
+        }
+    };
+
 };
